9ab26cc757d45753e5eabc5078864777247f93b7
Kseniya Pistsova <ksushapistsova@gmail.com>
2025-02-19 20:59:36 +0300

    feat: add scaling and oversampling function in utils.py

---
+++ src/utils.py
@@ -0,0 +1,41 @@
+# src/utils.py
+
+from sklearn.preprocessing import StandardScaler
+from imblearn.over_sampling import RandomOverSampler
+import numpy as np
+
+
+def scale_and_oversample(df, oversample=False):
+    """
+    Масштабирует признаки и применяет RandomOverSampler для балансировки классов.
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        Датасет с признаками и целевой переменной (class). Целевая переменная должна быть последней колонкой.
+    oversample : bool, optional
+        Если True, применяется RandomOverSampler для балансировки классов. По умолчанию False.
+
+    Returns
+    -------
+    tuple
+        - data (ndarray): Масштабированные данные с добавленной целевой переменной.
+        - X_scaled (ndarray): Масштабированные признаки.
+        - y (ndarray): Целевая переменная.
+    """
+
+    X = df[df.columns[:-1]].values  # Все колонки кроме последней
+    y = df[df.columns[-1]].values  # Последняя колонка - это целевая переменная (class)
+
+    # Масштабируем признаки
+    scaler = StandardScaler()
+    X, y = scaler.fit_transform(X, y)
+
+    if oversample:
+        # Применяем овер-сэмплинг
+        ros = RandomOverSampler(random_state=42)
+        X, y = ros.fit_resample(X, y)
+
+    data = np.hstack((X, np.reshape(y, (-1, 1))))
+
+    return data, X, y
